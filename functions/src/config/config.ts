35d723ec03bc90da420c69499c4e7b458b5c5a42
Jdevvv <llanta.jeremy@gmail.com>
2020-10-01 14:34:32 +0200

    fix

--- functions/src/config/config.ts
+++
@@ -1,8 +0,0 @@
-const serviceAccount = require('../permissions.json');
-
-import * as admin from 'firebase-admin';
-
-export const firebase = admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-	databaseURL: 'https://mdshat-1ae4d.firebaseio.com',
-});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,56 @@
 const functions = require('firebase-functions');
 import * as express from 'express';
 import * as cors from 'cors';
-// import { firebase } from './config/config';
+import * as admin from 'firebase-admin';
 
 const app = express();
 app.use(cors({ origin: true }));
 
-require('./endpoints/users.ts');
+const serviceAccount = require('../permissions.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: 'https://mdshat-1ae4d.firebaseio.com',
+});
+
+const actionCodeSettings = {
+	// URL you want to redirect back to. The domain (www.example.com) for
+	// this URL must be whitelisted in the Firebase Console.
+	url: '',
+	// This must be true for email link sign-in.
+	handleCodeInApp: true,
+	iOS: {
+		bundleId: 'fr.mds.mydigitalshat',
+	},
+	android: {
+		packageName: 'fr.mds.mydigitalshat',
+		installApp: true,
+		minimumVersion: '12',
+	},
+	// FDL custom domain.
+	dynamicLinkDomain: 'coolapp.page.link',
+};
+
+app.post('/api/users', (req: any, res: any) => {
+	admin
+		.auth()
+		.createUser({
+			email: req.body.email,
+			emailVerified: false,
+			password: req.body.password,
+		})
+		.then(function (userRecord) {
+			const verificationLink = admin
+				.auth()
+				.generateEmailVerificationLink(
+					req.body.email,
+					actionCodeSettings
+				);
+			console.log(verificationLink);
+		})
+		.catch(function (error) {
+			console.log('Error creating new user:', error);
+		});
+});
 
 exports.app = functions.https.onRequest(app);
