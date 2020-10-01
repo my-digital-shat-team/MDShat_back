ef4a0737085ebbe16bf4ce2178db619486d794de
Mayeut <mayeut@gmail.com>
2020-10-01 11:21:56 +0200

    jecpaskoametre

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,9 +3,16 @@ const admin = require('firebase-admin');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const serviceAccount = require("./permissions.json");
 app.use(cors({ origin: true }));
 
-app.get('/hello-world', (req, res) => {
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mdshat-1ae4d.firebaseio.com"
+});
+const db = admin.firestore();
+
+app.get('/hello-world', (req: any, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): any; new(): any; }; }; }) => {
   return res.status(200).send('Hello World!');
 });
 
