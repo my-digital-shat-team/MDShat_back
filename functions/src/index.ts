274880d4adaf006434f91f227bf36ed9c202552d
Jdevvv <llanta.jeremy@gmail.com>
2020-10-01 15:16:35 +0200

    some endpoints

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,6 +12,7 @@ admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount),
 	databaseURL: 'https://mdshat-1ae4d.firebaseio.com',
 });
+const db = admin.firestore();
 
 const actionCodeSettings = {
 	// URL you want to redirect back to. The domain (www.example.com) for
@@ -30,6 +31,8 @@ const actionCodeSettings = {
 	// FDL custom domain.
 	dynamicLinkDomain: 'coolapp.page.link',
 };
+
+////// USERS ///////
 
 app.post('/api/users', (req: any, res: any) => {
 	admin
@@ -51,6 +54,104 @@ app.post('/api/users', (req: any, res: any) => {
 		.catch(function (error) {
 			console.log('Error creating new user:', error);
 		});
+});
+
+app.get('/api/users/:uid', (req: any, res: any) => {
+	admin
+		.auth()
+		.getUser(req.params.uid)
+		.then(function (userRecord) {
+			console.log('Successfully fetched user data:', userRecord.toJSON());
+			res.status(200).send(userRecord);
+		})
+		.catch(function (error) {
+			res.stats(500).send('Error fetching user data:', error);
+		});
+});
+
+////// CHAT ///////
+
+app.post('/api/messages', (req, res) => {
+	(async () => {
+		try {
+			await db
+				.collection('messages')
+				.doc('/' + req.body.id + '/')
+				.create({
+					user_id: req.body.user_id,
+					channel_id: req.body.channel_id,
+					content: req.body.content,
+				});
+			return res.status(200).send();
+		} catch (error) {
+			console.log(error);
+			return res.status(500).send(error);
+		}
+	})();
+});
+
+app.get('/api/messages/:id', (req, res) => {
+	(async () => {
+		try {
+			const document = db.collection('messages').doc(req.params.id);
+			let item = await document.get();
+			let response = item.data();
+			return res.status(200).send(response);
+		} catch (error) {
+			console.log(error);
+			return res.status(500).send(error);
+		}
+	})();
+});
+
+app.get('/api/messages/:user_id', async (req, res) => {
+	try {
+		const userQuerySnapshot = await db.collection('messages').get();
+		const messages: any[] = [];
+		userQuerySnapshot.forEach((doc) => {
+			messages.push({
+				id: doc.id,
+				data: doc.data(),
+			});
+		});
+		res.status(200).json(messages);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
+
+////// CHANNEL ///////
+
+app.post('/api/channels', (req, res) => {
+	(async () => {
+		try {
+			await db
+				.collection('channels')
+				.doc('/' + req.body.id + '/')
+				.create({
+					name: req.body.name,
+					is_private: req.body.is_private,
+				});
+			return res.status(200).send();
+		} catch (error) {
+			console.log(error);
+			return res.status(500).send(error);
+		}
+	})();
+});
+
+app.get('/api/channels/:id', (req, res) => {
+	(async () => {
+		try {
+			const document = db.collection('channels').doc(req.params.id);
+			let item = await document.get();
+			let response = item.data();
+			return res.status(200).send(response);
+		} catch (error) {
+			console.log(error);
+			return res.status(500).send(error);
+		}
+	})();
 });
 
 exports.app = functions.https.onRequest(app);
