c2ca6990e55acf6aa7c15457d32debd4340e2298
Mayeut <mayeut@gmail.com>
2020-10-01 10:53:37 +0200

    adds express and cors udpdates gitignore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,12 @@
-import * as functions from 'firebase-functions';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors({ origin: true }));
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.get('/hello-world', (req, res) => {
+  return res.status(200).send('Hello World!');
+});
+
+exports.app = functions.https.onRequest(app);
